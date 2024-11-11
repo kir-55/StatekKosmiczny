c46e48341dc78002caa389c17f4307b8a6b3306c
Wojciech Masojć <wotmas09@gmail.com>
2024-11-11 12:15:43 +0100

    bezwladnosc statku i więcej:

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,22 +1,29 @@
 extends CharacterBody2D
 
-
 @export var root_node: Node
 
-@export var speed = 300.0
+@export var speed: float = 300.0  # Maksymalna prędkość w osi X
+@export var y_speed: float = 500.0  # Prędkość w osi Y
 
-@export var y_speed = 500.0
-
+@export var smoothing_factor: float = 0.05  # Miękkość zmiany kierunku (0.0 - natychmiastowo, 1.0 - brak ruchu)
 
 func _physics_process(delta):
 	var direction = Input.get_axis("left", "right")
 	
-	if direction:
-		velocity.x = direction * speed
-	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
+	# Pryzspieszanie samolotu
+	y_speed += delta * 10
+	
+	# Oblicz docelową prędkość w osi X
+	var target_velocity_x = direction * speed
+	
+	# Płynne przejście (smoothing)
+	velocity.x = lerp(velocity.x, target_velocity_x, smoothing_factor)
+
+	# Ruch w osi Y (np. upadek)
 	velocity.y = y_speed * -1
 
+	# Przesuń postać
 	move_and_slide()
+
 	
 
